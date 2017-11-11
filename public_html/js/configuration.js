aa9ee8aa1641a078d0ace2b041c2e8f03df6800d
fr33z00 <frisou76@yahoo.fr>
2017-11-11 11:27:10 +0100

    Scission de la licence et des remerciements en 2 pages

--- public_html/js/configuration.js
+++ public_html/js/configuration.js
@@ -34,6 +34,9 @@ $(document).ready(function() {
   else if (document.URL.indexOf("#ca") >= 0) {
     $($("ul.nav-tabs li")[2]).children("a")[0].click();
   } 
+  else if (document.URL.indexOf("#licence") >= 0) {
+    $($("ul.nav-tabs li")[3]).children("a")[0].click();
+  } 
 
   // extract by primary key
   $("button.edit-by-prim-key").on("click", function(e) {
