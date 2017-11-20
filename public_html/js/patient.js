04fce7849879415e56ba2529a0a71520537f6817
Bertrand Boutillier <b.boutillier@gmail.com>
2017-11-20 22:55:58 +0100

    retrait fermeture zone mail

--- public_html/js/patient.js
+++ public_html/js/patient.js
@@ -182,12 +182,6 @@ $(document).ready(function() {
     $(window).unbind("beforeunload");
   });
 
-  //close button zone newMail
-  $('body').on("click", "#cleanNewMail", function(e) {
-    $('#newMail').html('');
-    $(window).unbind("beforeunload");
-  });
-
   ////////////////////////////////////////////////////////////////////////
   ///////// Observations spécifiques aux lignes de l'historique  (dont modal)
 
