684d7a4983e016c007cd5759046a0f5035962217
Bertrand Boutillier <b.boutillier@gmail.com>
2017-11-22 16:16:57 +0100

    corrections sql mineure

--- sqlUpgrade.sql
+++ sqlUpgrade.sql
@@ -5,6 +5,8 @@
 ALTER TABLE actes_base MODIFY `tarifs1` float DEFAULT NULL;
 ALTER TABLE actes_base MODIFY `tarifs2` float DEFAULT NULL;
 ALTER TABLE actes MODIFY `details` text DEFAULT NULL;
+ALTER TABLE data_types CHANGE `formType` `formType` ENUM('','date','email','lcc','number','select','submit','tel','text','textarea','checkbox') NOT NULL DEFAULT '';
+UPDATE data_types set formType='checkbox' where id in ('436','492','493','496');
 
 -- 1.4.0 to 2.0.0
 
