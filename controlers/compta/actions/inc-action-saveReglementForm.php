63ac9d651c6f9038be04e70356b04b02658d0a93
fr33z00 <frisou76@yahoo.fr>
2018-02-10 10:00:51 +0100

    Amélioration du code, et ajout/suppression automatique du drapeau 'important' sur les règlements incomplets

--- controlers/compta/actions/inc-action-saveReglementForm.php
+++ controlers/compta/actions/inc-action-saveReglementForm.php
@@ -24,6 +24,7 @@
  * Compta > action : sauver un réglement
  *
  * @author Bertrand Boutillier <b.boutillier@gmail.com>
+ * @contrib fr33z00 <https://www.github.com/fr33z00>
  */
 
 
@@ -34,24 +35,14 @@ if (count($_POST)>0) {
 
     $supportID = $_POST['objetID'];
 
-    //cheque
-    if (!isset($_POST['regleCheque'])) {
-        $_POST['regleCheque']='';
+    foreach (['regleCheque', 'regleCB', 'regleEspeces', 'regleTiersPayeur', 'regleIdentiteCheque'] as $param) {
+        if (!isset($_POST[$param])) {
+          $_POST[$param]='';
+        }
     }
 
-    //cb
-    if (!isset($_POST['regleCB'])) {
-        $_POST['regleCB']='';
-    }
-
-    //espèces
-    if (!isset($_POST['regleEspeces'])) {
-        $_POST['regleEspeces']='';
-    }
-
-    if (!isset($_POST['regleIdentiteCheque'])) {
-        $_POST['regleIdentiteCheque']='';
-    }
+    $important=array('id'=>$supportID, 'important'=>($_POST['regleCheque']+$_POST['regleCB']+$_POST['regleEspeces']) < $_POST['apayer']?'y':'n');
+    msSQL::sqlInsert('objets_data', $important);
     
     if (($_POST['regleCheque']+$_POST['regleCB']+$_POST['regleEspeces']) <= $_POST['apayer']) {
         $patient->createNewObjetByTypeName('regleCheque', $_POST['regleCheque']+$_POST['dejaCheque'], $supportID);
