b5c486783a0a2895808cbf8da77ddf96a554916a
fr33z00 <frisou76@yahoo.fr>
2018-02-23 19:15:08 +0100

    Correction sur tableau des valeurs pour agendaPatientsOfTheDay, le merge changeant les index

--- controlers/configuration/configSpecificUserParam.php
+++ controlers/configuration/configSpecificUserParam.php
@@ -45,7 +45,8 @@
          if (array_key_exists('name', $v) and ($v['name'] =='agendaNumberForPatientsOfTheDay' or $v['name'] == 'administratifComptaPeutVoirRecettesDe')) {
              $v['formValues']=msSQL::sql2tabKey("SELECT id, name FROM people WHERE name!='' and type='pro'", "id", "name");
              if ($v['name'] == 'agendaNumberForPatientsOfTheDay') {
-                 $v['formValues']=array_merge(array('0'=>''), $v['formValues']);
+                 $v['formValues']['0']='';
+                 ksort($v['formValues']);
              }
              if ($v['name'] == 'administratifComptaPeutVoirRecettesDe') {
                  $v['userVal']=explode(',', $v['userVal']);
