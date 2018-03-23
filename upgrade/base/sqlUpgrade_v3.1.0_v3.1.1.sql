df00c89b1324ce69cf2d74d9ea1f0db26fc00e4d
fr33z00 <frisou76@yahoo.fr>
2018-03-23 12:54:29 +0100

    correction de la version en bdd

--- upgrade/base/sqlUpgrade_v3.1.0_v3.1.1.sql
+++ upgrade/base/sqlUpgrade_v3.1.0_v3.1.1.sql
@@ -1,4 +1,4 @@
 UPDATE `data_types` SET `description`='Ordonnance simple' WHERE `name`='ordoPorteur';
 UPDATE `data_types` SET `description`='Règlement secteurs 1 et 2' WHERE `name`='reglePorteur';
 
-UPDATE `system` SET `value`='3.1.1' WHERE `groupe`='module' and `name`='base';
+UPDATE `system` SET `value`='v3.1.1' WHERE `groupe`='module' and `name`='base';
