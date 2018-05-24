9595141edb39ac4197af55edfa0f81fc1f968cf5
Bertrand Boutillier <b.boutillier@gmail.com>
2018-05-24 14:51:04 +0200

    correction du processus d'upgrade

--- upgrade/base/sqlUpgrade_v3.1.2_v4.0.0_post.php
+++ upgrade/base/sqlUpgrade_v3.1.2_v4.0.0_post.php
@@ -1,31 +1,35 @@
 <?php
 $initialDir=getcwd();
 
-msSQL::sqlQuery("INSERT INTO configuration (name, level, value) VALUES
-  ('mailRappelLogCampaignDirectory', 'default', '".$p['config']['mailRappelLogCampaignDirectory']."'),
-  ('smsLogCampaignDirectory', 'default', '".$p['config']['smsLogCampaignDirectory']."'),
-  ('apicryptCheminInbox', 'default', '".$p['config']['apicryptCheminInbox']."'),
-  ('apicryptCheminArchivesInbox', 'default', '".$p['config']['apicryptCheminArchivesInbox']."'),
-  ('apicryptCheminFichierNC', 'default', '".$p['config']['apicryptCheminFichierNC']."'),
-  ('apicryptCheminFichierC', 'default', '".$p['config']['apicryptCheminFichierC']."'),
-  ('apicryptCheminVersClefs', 'default', '".$p['config']['apicryptCheminVersClefs']."'),
-  ('apicryptCheminVersBinaires', 'default', '".$p['config']['apicryptCheminVersBinaires']."'),
-  ('dicomWorkListDirectory', 'default', '".$p['config']['dicomWorkListDirectory']."'),
-  ('dicomWorkingDirectory', 'default', '".$p['config']['dicomWorkingDirectory']."'),
-  ('templatesPdfFolder', 'default', '".$p['config']['templatesPdfFolder']."')
-  ON DUPLICATE KEY UPDATE value=VALUES(value)");
+$varQuiRestentDansYml = ['protocol', 'host', 'urlHostSuffixe', 'webDirectory', 'stockageLocation', 'backupLocation',
+  'workingDirectory', 'cookieDomain', 'cookieDuration', 'fingerprint', 'sqlServeur', 'sqlBase', 'sqlUser', 'sqlPass',
+  'sqlVarPassword', 'templatesFolder', 'twigEnvironnementCache', 'twigEnvironnementAutoescape'];
+
+$oldVarToIgnore = ['twigPdfTemplatesDir', 'templateBaseFolder', 'homeDirectory'];
+
+if (is_file($p['homepath'].'config/config.yml')) {
+  $oldp=Spyc::YAMLLoad($p['homepath'].'config/config.yml');
+  foreach($oldp as $k=>$v) {
+
+    if($k=='dicomAutoSendPatient2Echo') $k='dicomAutoSendPatient';
+    if($k=='apicryptPopPass') $k='apicryptPopPassword';
+    if($k=='ecofaxPass') $k='ecofaxPassword';
+
+    if(!in_array($k, $varQuiRestentDansYml) and !in_array($k, $oldVarToIgnore)) {
+      msSQL::sqlQuery("INSERT INTO configuration (name, level, value) VALUES ('".$k."', 'default', '".$v."') ON DUPLICATE KEY UPDATE value='".$v."'");
+    }
+  }
+}
 
 $conf=array();
-foreach(['protocol', 'host', 'urlHostSuffixe', 'webDirectory', 'stockageLocation', 'backupLocation', 
-  'workingDirectory', 'cookieDomain', 'cookieDuration', 'fingerprint', 'sqlServeur', 'sqlBase', 'sqlUser', 'sqlPass', 
-  'sqlVarPassword', 'templatesFolder', 'twigEnvironnementCache', 'twigEnvironnementAutoescape'] as $k) {
+foreach($varQuiRestentDansYml as $k) {
     $conf[$k]=$p['config'][$k];
 };
 if (is_file($p['homepath'].'config/config.yml'))
     rename($p['homepath'].'config/config.yml', $p['homepath'].'config/config.yml.bak');
 file_put_contents($p['homepath'].'config/config.yml', Spyc::YAMLDump($conf, false, 0, true));
 
-$agendaFiles=glob($p['webDirectory'].'/agendasConfigurations/configAgenda*.js');
+$agendaFiles=glob($p['config']['webDirectory'].'/agendasConfigurations/configAgenda*.js');
 foreach($agendaFiles as $file) {
     rename($file, str_replace($p['webDirectory'].'/agendasConfigurations/configAgenda', $p['homepath'].'config/agendas/agenda', $file));
 }
@@ -43,4 +47,3 @@ if (exec('COMPOSER_HOME="/tmp/" composer.phar -V', $ret) and strpos($ret, 'Compo
 chdir($p['config']['webDirectory']);
 exec('COMPOSER_HOME="/tmp/" '.$pathToComposer.' update 2>&1', $output);
 chdir($initialDir);
-
