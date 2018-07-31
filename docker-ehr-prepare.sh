10288d5cd66e59218beac0ad42a13a4864c2f6ff
Jean-Baptiste BUGEAUD <bugeaud@gmail.com>
2018-07-31 14:52:57 +0000

    Allow prepare script to go thru on non dhclient setup

--- docker-ehr-prepare.sh
+++ docker-ehr-prepare.sh
@@ -6,16 +6,19 @@ echo "$(tput setaf 10)DONE$(tput sgr0) File structure checked"
 # See https://github.com/michaelgrosner/tribeca/issues/184
 # This issue was noted Closed, but is actually not fixed as per mid-2018
 export DAEMON_CONF=/etc/docker/daemon.json
-export DNS_SERVER_REAL_FIRST=`cat /var/lib/dhcp/dhclient*.leases | grep dhcp-server-identifier | grep -m1 -Eow "([^ ]*);$" | sed s'/.$//'`
-if grep -q $DNS_SERVER_REAL_FIRST $DAEMON_CONF ;  then
-  echo "$(tput setaf 11)TODO$(tput sgr0) $DNS_SERVER_REAL_FIRST DNS Looks like already set in $DAEMON_CONF. Please make sure it is set as first DNS server in the dns property." 
-else
- #So, let's fix it
- if [ -s $DAEMON_CONF ]; then
-  echo "$(tput setaf 11)TODO$(tput sgr0) Please add manually the DNS server $DNS_SERVER_REAL_FIRST value to the file $DAEMON_CONF in the dns property at first position of the JSON array."
+if test -n "$(find /var/lib/dhcp/ -maxdepth 1 -name 'dhclient*.leases' -print -quit)" ; then
+ export DNS_SERVER_REAL_FIRST=`cat /var/lib/dhcp/dhclient*.leases | grep dhcp-server-identifier | grep -m1 -Eow "([^ ]*);$" | sed s'/.$//'`
+ if grep -q $DNS_SERVER_REAL_FIRST $DAEMON_CONF ;  then
+   echo "$(tput setaf 11)TODO$(tput sgr0) $DNS_SERVER_REAL_FIRST DNS Looks like already set in $DAEMON_CONF. Please make sure it is set as first DNS server in the dns property." 
  else
-  echo "{ \"dns\": [\"$DNS_SERVER_REAL_FIRST\", \"8.8.8.8\", \"9.9.9.9\"] }" > $DAEMON_CONF
-  echo "$(tput setaf 10)DONE$(tput sgr0) Creating $DAEMON_CONF file with a fixed DNS Server $DNS_SERVER_REAL_FIRST to prevent Docker DNS issues EAI_RETRY"
+  #So, let's fix it
+  if [ -s $DAEMON_CONF ]; then
+   echo "$(tput setaf 11)TODO$(tput sgr0) Please add manually the DNS server $DNS_SERVER_REAL_FIRST value to the file $DAEMON_CONF in the dns property at first position of the JSON array."
+  else
+   echo "{ \"dns\": [\"$DNS_SERVER_REAL_FIRST\", \"8.8.8.8\", \"9.9.9.9\"] }" > $DAEMON_CONF
+   echo "$(tput setaf 10)DONE$(tput sgr0) Creating $DAEMON_CONF file with a fixed DNS Server $DNS_SERVER_REAL_FIRST to prevent Docker DNS issues EAI_RETRY"
+  fi
  fi
+else
+ echo "$(tput setaf 11)WARN$(tput sgr0) Not able to update $DAEMON_CONF as your DHCP configuration is currently not supported by this script. If Docker generate some DNS issues EAI_RETRY, please open an issue with details of your distribution & environment."
 fi
-
