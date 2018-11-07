0a450837da138200bb37ea1095751b7999b3ffc6
Bertrand Boutillier <b.boutillier@gmail.com>
2018-11-07 14:20:28 +0100

    nouveau : menu supérieur de navigation par textes ou icones

--- upgrade/base/sqlUpgrade_v4.2.1_v4.3.0.sql
+++ upgrade/base/sqlUpgrade_v4.2.1_v4.3.0.sql
@@ -24,5 +24,9 @@ INSERT IGNORE INTO `configuration`(`name`, `cat`, `level`, `type`, `description`
 -- Formulaire nouveau patient (+ num de sécu)
 update `forms` set `yamlStructure`= 'structure:\r\n  row1:                              \r\n    col1:                              \r\n      head: \'Etat civil\'             \r\n      size: 4\r\n      bloc:                          \r\n        - administrativeGenderCode                 		#14   Sexe\n        - birthname,required,autocomplete,data-acTypeID=lastname:birthname 		#1    Nom de naissance\n        - lastname,autocomplete,data-acTypeID=lastname:birthname 		#2    Nom d usage\n        - firstname,required,autocomplete,data-acTypeID=firstname:othersfirstname:igPrenomFA:igPrenomFB:igPrenomFC 		#3    Prénom\n        - birthdate,class=pick-year                		#8    Date de naissance\n    col2:\r\n      head: \'Contact\'\r\n      size: 4\r\n      bloc:\r\n        - personalEmail                            		#4    Email personnelle\n        - mobilePhone                              		#7    Téléphone mobile\n        - homePhone                                		#10   Téléphone domicile\n        - nss                                      		#180  Numéro de sécu\n    col3:\r\n      head: \'Adresse personnelle\'\r\n      size: 4\r\n      bloc: \r\n        - streetNumber                             		#9    n°\n        - street,autocomplete,data-acTypeID=street:rueAdressePro 		#11   Voie\n        - postalCodePerso                          		#13   Code postal\n        - city,autocomplete,data-acTypeID=city:villeAdressePro 		#12   Ville\n        - deathdate                                		#516  Date de décès\n  row2:\r\n    col1:\r\n      size: 12\r\n      bloc:\r\n        - notes,rows=5                             		#21   Notes' where internalName = "baseNewPatient";
 
-  -- Mise à jour n° de version
-  UPDATE `system` SET `value`='v4.3.0' WHERE `name`='base' and `groupe`='module';
+-- Paramètres de configuration : design du menu sup de navigation
+INSERT IGNORE INTO `configuration`(`name`, `cat`, `level`, `type`, `description`, `value`) VALUES
+('designTopMenuStyle', 'Ergonomie & design', 'default', 'icones / textes', 'aspect du menu de navigation du haut de page', 'textes');
+
+-- Mise à jour n° de version
+UPDATE `system` SET `value`='v4.3.0' WHERE `name`='base' and `groupe`='module';
