f171d0122ae2e7ae1771ee0f6378e4e6d689d00c
Bertrand Boutillier <b.boutillier@gmail.com>
2019-01-14 15:27:18 +0100

    fix: correction outil CDA correction et amélioration sur la génération des possibilités

--- controlers/configuration/configFormCdaTools.php
+++ controlers/configuration/configFormCdaTools.php
@@ -27,116 +27,116 @@
  */
 
 
- function treatDataType(&$t) {
-   global $formc;
-   if($t['formType'] == 'select') {
-         $t['formValues']=Spyc::YAMLLoad($t['formValues']);
-         $t['formValues']=array_filter($t['formValues']);
-         $t['keyValues']=array_keys($t['formValues']);
-       } elseif($t['formType'] == 'number') {
-         $padat=$formc->getDataTypeFormParams($t['name']);
-         if(isset($padat['min'],$padat['max'],$padat['step'])) {
-           for($i=$padat['min'];$i<=$padat['max'];$i=$i+$padat['step']) {
-             $tab[$i]=$i;
-           }
-           $t['formValues']=(array)$tab;
+function treatDataType(&$t) {
+  global $formc;
+  if($t['formType'] == 'select') {
+    $t['formValues']=Spyc::YAMLLoad($t['formValues']);
+    $t['formValues']=array_filter($t['formValues']);
+    $t['keyValues']=array_keys($t['formValues']);
+  } elseif($t['formType'] == 'number') {
+    $padat=$formc->getDataTypeFormParams($t['name']);
+    if(isset($padat['min'],$padat['max'],$padat['step'])) {
+      for($i=$padat['min'];$i<=$padat['max'];$i=$i+$padat['step']) {
+        $tab[$i]=$i;
+      }
+      $t['formValues']=(array)$tab;
+    }
+    $t['formValues']=array_filter($t['formValues']);
+    $t['keyValues']=array_keys($t['formValues']);
+  }
+}
+
+function concat(array $array, $clef) {
+   $current = array_shift($array);
+   if(count($array) > 0) {
+       $results = array();
+       $temp = concat($array, $clef);
+       foreach($current[$clef] as $word) {
+         foreach($temp as $value) {
+           $results[] =  $word . '|' . $value;
          }
-         $t['formValues']=array_filter($t['formValues']);
-         $t['keyValues']=array_keys($t['formValues']);
        }
+       return $results;
+   }
+   else {
+      return $current[$clef];
+   }
+}
+
+
+//admin uniquement
+if (!msUser::checkUserIsAdmin()) {
+   $template="forbidden";
+} else {
+   $debug='';
+   $template="configFormCdaTools";
+   $p['page']['formID']=$match['params']['form'];
+   if (!is_numeric($p['page']['formID'])) {
+       die();
    }
 
- //admin uniquement
- if (!msUser::checkUserIsAdmin()) {
-     $template="forbidden";
- } else {
-     $debug='';
-     $template="configFormCdaTools";
-     $p['page']['formID']=$match['params']['form'];
-     if (!is_numeric($p['page']['formID'])) {
-         die();
-     }
 
+  //sortie du formulaire et préparation
+  if ($formData=msSQL::sqlUnique("select * from forms where id='".$p['page']['formID']."' limit 1")) {
 
-    //sortie du formulaire et préparation
-    if ($formData=msSQL::sqlUnique("select * from forms where id='".$p['page']['formID']."' limit 1")) {
+    $formc= new msForm;
+    $formc->setFormID($p['page']['formID']);
 
-      $formc= new msForm;
-      $formc->setFormID($p['page']['formID']);
+    $formData['cda']=Spyc::YAMLLoad($formData['cda']);
 
-      $formData['cda']=Spyc::YAMLLoad($formData['cda']);
+    if(isset($formData['cda']['actesPossibles'],$formData['cda']['clinicalDocument']['documentationOf']['serviceEvent']['paramConditionServiceEvent'])) {
 
-      if(isset($formData['cda']['actesPossibles'],$formData['cda']['clinicalDocument']['documentationOf']['serviceEvent']['paramConditionServiceEvent'])) {
+      // associations déjà en place
+      $p['page']['deja']=$formData['cda']['clinicalDocument']['documentationOf']['serviceEvent']['code'];
 
-        // associations déjà en place
-        $p['page']['deja']=$formData['cda']['clinicalDocument']['documentationOf']['serviceEvent']['code'];
+      // actes possibles définis
+      $p['page']['actesPossibles']=$formData['cda']['actesPossibles'];
 
-        // actes possibles définis
-        $p['page']['actesPossibles']=$formData['cda']['actesPossibles'];
+      // paramètres concernés
+      $p['page']['paramsPossibles']=(array)$formData['cda']['clinicalDocument']['documentationOf']['serviceEvent']['paramConditionServiceEvent'];
 
-        // paramètres concernés
-        $p['page']['paramsPossibles']=(array)$formData['cda']['clinicalDocument']['documentationOf']['serviceEvent']['paramConditionServiceEvent'];
-
-        // sortie data de chaque param
-        $data = new msData;
-        foreach($p['page']['paramsPossibles'] as $k=>$pa) {
-          if(is_string($pa)) {
-            $p['page']['paramsPossiblesData'][$k]=$data->getDataTypeByName($pa);
-            treatDataType($p['page']['paramsPossiblesData'][$k]);
-            $nbPo[$k]=count($p['page']['paramsPossiblesData'][$k]['formValues']);
-          } elseif (is_array($pa)) {
-            foreach($pa as $sk=>$spa) {
-              $p['page']['paramsPossiblesData'][$k][$sk]=$data->getDataTypeByName($spa);
-              treatDataType($p['page']['paramsPossiblesData'][$k][$sk]);
-              if(isset($nbPo[$k])) {
-                $nbPo[$k]=$nbPo[$k]+count($p['page']['paramsPossiblesData'][$k][$sk]['formValues']);
-              } else {
-                $nbPo[$k]=count($p['page']['paramsPossiblesData'][$k][$sk]['formValues']);
-              }
-            }
+      // sortie data de chaque param
+      $data = new msData;
+      foreach($p['page']['paramsPossibles'] as $k=>$pa) {
+        if(is_string($pa)) {
+          $p['page']['paramsPossiblesData'][$k]=$data->getDataTypeByName($pa);
+          treatDataType($p['page']['paramsPossiblesData'][$k]);
+        } elseif (is_array($pa)) {
+          foreach($pa as $sk=>$spa) {
+            $p['page']['paramsPossiblesData'][$k][$sk]=$data->getDataTypeByName($spa);
+            treatDataType($p['page']['paramsPossiblesData'][$k][$sk]);
           }
         }
-
-        // nombre de possibilités
-        $nbPo=array_filter($nbPo);
-        $nbPo=array_product($nbPo);
-
-        // on regroupe
-        foreach($p['page']['paramsPossiblesData'] as $k=>$v) {
-            if(isset($v['label'])) {
-              foreach($v['keyValues'] as $kv) {
-                $tab[$k]['asso'][$v['name'].'@'.$kv]=$v['formValues'][$kv];
-              }
-            } else {
-              foreach($v as $k2=>$v2)
-                foreach($v2['keyValues'] as $kv2) {
-                  $tab[$k]['asso'][$v2['name'].'@'.$kv2]=$v2['formValues'][$kv2];
-              }
-            }
-            $tab[$k]['clefs']=array_keys($tab[$k]['asso']);
-            $tab[$k]['values']=array_values($tab[$k]['asso']);
-        }
-
-        // on génère le tableau final pour exploitation par twig
-        foreach($tab as $k=>$v) {
-          for($i=0;$i<$nbPo;$i++) {
-            $nblocal=count($v['clefs']);
-            if(!isset($tabr[$i])) {
-              $tabr[$i]['clef']=$v['clefs'][($i%$nblocal)];
-            } else {
-              $tabr[$i]['clef'].='|'.$v['clefs'][($i%$nblocal)];
-            }
-            $tabr[$i]['values'][$k]=$v['values'][($i%$nblocal)];
-          }
-        }
-
-        sort($tabr);
-        $p['page']['dataTab']=$tabr;
       }
 
+      // on regroupe
+      foreach($p['page']['paramsPossiblesData'] as $k=>$v) {
+          if(isset($v['label'])) {
+            foreach($v['keyValues'] as $kv) {
+              $tab[$k]['asso'][$v['name'].'@'.$kv]=$v['formValues'][$kv];
+            }
+          } else {
+            foreach($v as $k2=>$v2)
+              foreach($v2['keyValues'] as $kv2) {
+                $tab[$k]['asso'][$v2['name'].'@'.$kv2]=$v2['formValues'][$kv2];
+            }
+          }
+          $tab[$k]['clefs']=array_keys($tab[$k]['asso']);
+          $tab[$k]['values']=array_values($tab[$k]['asso']);
+      }
 
-      // jeux de valeurs
-      $p['page']['jdvClinicalDocumentCode']=msExternalData::getJdvDataFromXml('JDV_J07-XdsTypeCode_CI-SIS.xml');
-
+      $tabclefs=concat($tab, 'clefs');
+      $tablabel=concat($tab, 'values');
+      foreach($tablabel as $k=>$v) {
+        $tabr[$k]['clef']=$tabclefs[$k];
+        $tabr[$k]['values']=explode('|', $v);
+      }
+      sort($tabr);
+      $p['page']['dataTab']=$tabr;
     }
- }
+
+    // jeux de valeurs
+    $p['page']['jdvClinicalDocumentCode']=msExternalData::getJdvDataFromXml('JDV_J07-XdsTypeCode_CI-SIS.xml');
+
+  }
+}
