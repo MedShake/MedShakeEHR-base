648b4a958ba42e0d51363846b6b5a7df3894f9e0
Bertrand Boutillier <b.boutillier@gmail.com>
2019-01-21 20:31:05 +0100

    feat: log ajout userID

--- controlers/logs/logs.php
+++ controlers/logs/logs.php
@@ -48,7 +48,7 @@ if (isset($match['params']['instance'])) {
 }
 
 $ids = msData::getTypeIDsFromName(['firstname', 'lastname', 'birthname']);
-$p['page']['logs']=msSQL::sql2tab("select pd.* , TRIM(CONCAT(COALESCE(f.value,''), ' ', TRIM(CONCAT(COALESCE(l.value, ''), ' ', COALESCE(b.value,''))))) as prescripteur, t.label, t.groupe, t.name
+$p['page']['logs']=msSQL::sql2tab("select pd.* , TRIM(CONCAT(COALESCE(f.value,''), ' ', TRIM(CONCAT(COALESCE(l.value, ''), ' ', COALESCE(b.value,''))))) as prescripteur, CASE WHEN pd.byID!='' THEN pd.byID ELSE pd.fromID END as prescripteurID, t.label, t.groupe, t.name
 from objets_data as pd
 left join objets_data as f on f.toID=(CASE WHEN pd.byID!='' THEN pd.byID ELSE pd.fromID END) and f.typeID in (NULL, '', '".$ids['firstname']."') and f.outdated='' and f.deleted=''
 left join objets_data as l on l.toID=(CASE WHEN pd.byID!='' THEN pd.byID ELSE pd.fromID END) and l.typeID in (NULL, '', '".$ids['lastname']."') and l.outdated='' and l.deleted=''
@@ -57,7 +57,6 @@ left join data_types as t on t.id=pd.typeID
 where 1 $patientSel $typeSel $instance
 group by pd.id,t.id, f.id, b.id, l.id
 order by id desc limit 2000");
-
 
 if (isset($match['params']['patient'])) {
     $p['page']['patientID']=$match['params']['patient'];
