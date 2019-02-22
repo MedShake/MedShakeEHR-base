aa8ea081101d6c847597114c26ff364942a2ed24
Bertrand Boutillier <b.boutillier@gmail.com>
2019-02-22 11:39:19 +0100

    fix: refresh col latérale sur base seule

---
+++ controlers/module/base/patient/actions/patientAjax.php
@@ -0,0 +1,40 @@
+<?php
+/*
+ * This file is part of MedShakeEHR.
+ *
+ * Copyright (c) 2019
+ * Bertrand Boutillier <b.boutillier@gmail.com>
+ * http://www.medshake.net
+ *
+ * MedShakeEHR is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * MedShakeEHR is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MedShakeEHR.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * Patient : les requêtes ajax complémentaires du module base
+ *
+ * @author Bertrand Boutillier <b.boutillier@gmail.com>
+ */
+
+
+$debug='';
+$m=$match['params']['m'];
+
+$acceptedModes=array(
+    'refreshLatColPatientAtcdData', //rafraichir la colonne atcd
+);
+
+//inclusion
+if(is_file($p['homepath'].'controlers/module/base/patient/actions/inc-ajax-'.$m.'.php')) {
+   include('inc-ajax-'.$m.'.php');
+}
