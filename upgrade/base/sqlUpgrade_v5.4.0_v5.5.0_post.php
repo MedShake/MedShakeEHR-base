61e1e88227d36027d1df1a3a395cc90458b0f7a3
Bertrand Boutillier <b.boutillier@gmail.com>
2019-05-14 12:41:43 +0200

    fix: retrait du bower.json qui pouvait trainer dans public_html

--- upgrade/base/sqlUpgrade_v5.4.0_v5.5.0_post.php
+++ upgrade/base/sqlUpgrade_v5.4.0_v5.5.0_post.php
@@ -3,3 +3,5 @@
 // retrait des anciens fichiers JS formulaire
 @unlink($p['config']['webDirectory'].'/js/module/formsScripts/baseATCD.js');
 @unlink($p['config']['webDirectory'].'/js/module/formsScripts/baseSynthese.js');
+
+@unlink($p['config']['webDirectory'].'/bower.json');
