10e37be84b40617e0b7f5f22b39922eb57bf3ca3
Bertrand Boutillier <b.boutillier@gmail.com>
2019-05-14 17:42:35 +0200

    feat: forcer dernière version de composer.phar

--- upgrade/base/sqlUpgrade_v5.3.0_v5.4.0_post.php
+++ upgrade/base/sqlUpgrade_v5.3.0_v5.4.0_post.php
@@ -3,11 +3,8 @@
 $initialDir=getcwd();
 
 $pathToComposer='php '.$p['homepath'].'composer.phar';
-if (exec('COMPOSER_HOME="/tmp/" composer.phar -V', $ret) and strpos($ret, 'Composer version')!==false) {
-    $pathToComposer='composer.phar';
-} elseif (!is_file($pathToComposer)) {
-    file_put_contents($p['homepath'].'composer.phar', fopen("https://getcomposer.org/download/1.6.3/composer.phar", 'r'));
-}
+file_put_contents($p['homepath'].'composer.phar', fopen("https://getcomposer.org/download/1.8.5/composer.phar", 'r'));
+
 chdir($p['config']['webDirectory']);
 exec('COMPOSER_HOME="/tmp/" '.$pathToComposer.' update 2>&1', $output);
 chdir($p['homepath']);
