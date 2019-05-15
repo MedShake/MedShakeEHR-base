38818ac9478ca5691cb92a9da8a432f4903ed24a
Bertrand Boutillier <b.boutillier@gmail.com>
2019-05-15 09:57:37 +0200

    5.5.0

--- upgrade/base/sqlInstall.sql
+++ upgrade/base/sqlInstall.sql
@@ -774,4 +774,4 @@ INSERT IGNORE INTO `configuration` (`name`, `level`, `toID`, `module`, `cat`, `t
 
 INSERT IGNORE INTO `system` (`id`,`name`, `groupe`,`value`) VALUES
  (1, 'state', 'system', 'normal'),
- (2, 'base', 'module', 'v5.4.0');
+ (2, 'base', 'module', 'v5.5.0');
