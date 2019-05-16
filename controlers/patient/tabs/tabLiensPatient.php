93f95ec1c4fe3b2baf41f3d8a1d8da606c195c6f
Bertrand Boutillier <b.boutillier@gmail.com>
2019-05-16 14:41:05 +0200

    fix(patient): retrait code inutile

--- controlers/patient/tabs/tabLiensPatient.php
+++ controlers/patient/tabs/tabLiensPatient.php
@@ -31,8 +31,6 @@ $template="inc-patientPeopleRelations";
 
 $patient = new msPeople();
 $patient->setToID($match['params']['patientID']);
-$p['page']['patient']=$patient->getSimpleAdminDatas();
-$p['page']['patient']['id']=$match['params']['patientID'];
 
 //sortir les choix de relations patient<->prat
 $data = new msData();
