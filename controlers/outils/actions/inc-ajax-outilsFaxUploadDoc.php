895a71179966e651babc610b8f77e6ec68892bf9
Bertrand Boutillier <b.boutillier@gmail.com>
2019-05-17 14:48:05 +0200

    feat(outils): fax autonome possibilité d'envoyer un document externe par écofax OVH

---
+++ controlers/outils/actions/inc-ajax-outilsFaxUploadDoc.php
@@ -0,0 +1,45 @@
+<?php
+/*
+ * This file is part of MedShakeEHR.
+ *
+ * Copyright (c) 2019
+ * Bertrand Boutillier <b.boutillier@gmail.com>
+ * http://www.medshake.net
+ *
+ * MedShakeEHR is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * MedShakeEHR is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MedShakeEHR.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * Outils > ajax : upload par drag&drop de fichier PDF pour fax autonome
+ *
+ * @author Bertrand Boutillier <b.boutillier@gmail.com>
+ */
+
+
+
+$fichier=$_FILES['file'];
+$destinationDir=$p['config']['dicomWorkingDirectory'].$p['user']['id'].'/';
+msTools::checkAndBuildTargetDir($destinationDir);
+if(!is_dir($destinationDir)) die;
+
+$mimetype=msTools::getmimetype($fichier['tmp_name']);
+$acceptedtypes=array(
+  'application/pdf'=>'pdf',
+);
+if (array_key_exists($mimetype, $acceptedtypes)) {
+  $ext=$acceptedtypes[$mimetype];
+  $destination_file = $destinationDir.'pdf2fax.pdf';
+  move_uploaded_file($fichier['tmp_name'], $destination_file);
+}
+die();
