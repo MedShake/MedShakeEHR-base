a4cc2953058fed7f9b529efced75b2555bcb6c2d
Bertrand Boutillier <b.boutillier@gmail.com>
2019-07-26 09:03:05 +0200

    fix(SQL) : ajout IGNORE pour INSERT dans upgrade 5.9.0

--- upgrade/base/sqlUpgrade_v5.8.0_v5.9.0.sql
+++ upgrade/base/sqlUpgrade_v5.8.0_v5.9.0.sql
@@ -1,9 +1,9 @@
 -- Mise à jour n° de version
 UPDATE `system` SET `value`='v5.9.0' WHERE `name`='base' and `groupe`='module';
 
-ALTER TABLE `inbox` CHANGE `txtNumOrdre` `txtNumOrdre` INT(9) UNSIGNED NOT NULL; 
+ALTER TABLE `inbox` CHANGE `txtNumOrdre` `txtNumOrdre` INT(9) UNSIGNED NOT NULL;
 
 
-INSERT INTO `configuration` (`id`, `name`, `level`, `toID`, `module`, `cat`, `type`, `description`, `value`) VALUES (NULL, 'apicrypt2CertName', 'default', '0', '', 'Apicrypt', 'texte', 'nom du certificat à utiliser', '');
-INSERT INTO `configuration` (`id`, `name`, `level`, `toID`, `module`, `cat`, `type`, `description`, `value`) VALUES (NULL, 'apicrypt2CertPassword', 'default', '0', '', 'Apicrypt', 'texte', 'mot de passe du certificat', '');
-INSERT INTO `configuration` (`id`, `name`, `level`, `toID`, `module`, `cat`, `type`, `description`, `value`) VALUES (NULL, 'apicryptVersion', 'default', '0', '', 'Apicrypt', 'texte', 'version d\'Apicrypt à mettre en œuvre ', '1');
+INSERT IGNORE INTO `configuration` (`id`, `name`, `level`, `toID`, `module`, `cat`, `type`, `description`, `value`) VALUES (NULL, 'apicrypt2CertName', 'default', '0', '', 'Apicrypt', 'texte', 'nom du certificat à utiliser', '');
+INSERT IGNORE INTO `configuration` (`id`, `name`, `level`, `toID`, `module`, `cat`, `type`, `description`, `value`) VALUES (NULL, 'apicrypt2CertPassword', 'default', '0', '', 'Apicrypt', 'texte', 'mot de passe du certificat', '');
+INSERT IGNORE INTO `configuration` (`id`, `name`, `level`, `toID`, `module`, `cat`, `type`, `description`, `value`) VALUES (NULL, 'apicryptVersion', 'default', '0', '', 'Apicrypt', 'texte', 'version d\'Apicrypt à mettre en œuvre ', '1');
