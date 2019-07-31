a34d9211e77aed7b6932a44e581e5465406618f2
Bertrand Boutillier <b.boutillier@gmail.com>
2019-07-31 21:28:55 +0200

    feat: ajout d'un index sur date début table agenda

--- upgrade/base/sqlUpgrade_v5.9.0_v5.10.0.sql
+++ upgrade/base/sqlUpgrade_v5.9.0_v5.10.0.sql
@@ -1,3 +1,5 @@
 ALTER TABLE `agenda` ADD `attente` ENUM('non','oui') NOT NULL DEFAULT 'non' AFTER `absente`;
 ALTER TABLE `agenda_changelog` CHANGE `operation` `operation` ENUM('edit','move','delete','missing','waiting') CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL;
+ALTER TABLE `agenda` ADD INDEX (`start`, `userid`);
+
 INSERT INTO `configuration` (`name`, `level`, `toID`, `module`, `cat`, `type`, `description`, `value`) VALUES ('agendaRefreshDelayMenuPOTD', 'default', '0', '', 'Agenda', 'nombre', 'délai en secondes du rafraîchissement du menu Patients du jour - 0 pour jamais', '5');
