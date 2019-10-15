d1e809e012e7b484b484255457c780741be846e8
Bertrand Boutillier <b.boutillier@gmail.com>
2019-10-15 15:25:01 +0200

    fix: accès listing patient / prat user non admin

--- controlers/rechercher/patients.php
+++ controlers/rechercher/patients.php
@@ -64,14 +64,14 @@ if($p['config']['transmissionsPeutCreer'] == 'true') {
 if (msUser::checkUserIsAdmin()) {
   $p['page']['modules']=msModules::getInstalledModulesNames();
   $p['page']['userTemplates']=msConfiguration::getUserTemplatesList();
-}
 
-// Formulaire nouvel utilisateur
-$formModal = new msForm;
-$formModal->setFormIDbyName($p['page']['formModalIN']='baseNewUserFromPeople');
-$formModal->setOptionsForSelect(array(
-  'templates'=>[''=>'aucun'] + $p['page']['userTemplates'],
-  'modules'=>$p['page']['modules'],
-));
-$p['page']['formModal']=$formModal->getForm();
-$formModal->addHiddenInput($p['page']['formModal'], ['preUserID'=>'']);
+  // Formulaire nouvel utilisateur
+  $formModal = new msForm;
+  $formModal->setFormIDbyName($p['page']['formModalIN']='baseNewUserFromPeople');
+  $formModal->setOptionsForSelect(array(
+    'templates'=>[''=>'aucun'] + $p['page']['userTemplates'],
+    'modules'=>$p['page']['modules'],
+  ));
+  $p['page']['formModal']=$formModal->getForm();
+  $formModal->addHiddenInput($p['page']['formModal'], ['preUserID'=>'']);
+}
