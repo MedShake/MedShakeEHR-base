01325767fc4bcd39a7edb993979fb4152fb826aa
Bertrand Boutillier <b.boutillier@gmail.com>
2019-12-17 14:20:28 +0100

    feat(form): points entrée module par extension msForm permet d'agir sur le formulaire avant sa présentation écran

--- controlers/patient/actions/inc-ajax-extractCsForm.php
+++ controlers/patient/actions/inc-ajax-extractCsForm.php
@@ -36,12 +36,26 @@ if(!isset($_POST['csID'])) {
   $_POST['csID']=msSQL::sqlUniqueChamp("select id from data_types where groupe='typecs' and formValues='".msSQL::cleanVar($_POST['formIN'])."' limit 1");
 }
 
-//infos sur le form
+// formulaire
 $formInfos = new msData();
 $p['page']['formInfos'] = $formInfos->getDataType($_POST['csID'], array('label'));
 
-//formulaire
-$form = new msForm();
+// module du formulaire
+$formModule = new msForm;
+$p['page']['formInfos']['module']=$formModule->getFormUniqueRawField($formIN, 'module');
+
+// class du module étendant potentiellement msForm
+$class='msMod'.ucfirst($p['page']['formInfos']['module']).'Forms';
+$method_pre='doPreGetForm_'.$formIN;
+$method_post='doPostGetForm_'.$formIN;
+
+// instancier la bonne class pour travailler sur le form
+if(class_exists($class)) {
+  $form = new $class;
+} else {
+  $form = new msForm();
+}
+
 $form->setFormIDbyName($formIN);
 
 //chargement des values si demandé
@@ -52,6 +66,12 @@ if (isset($_POST['prevalues'])) {
         $form->getPrevaluesForPatient($_POST['patientID']);
     }
 }
+
+// méthode sépcifique au module et form : pre
+if(method_exists($class,$method_pre)) {
+  $form->$method_pre();
+}
+
 $p['page']['form']=$form->getForm();
 
 if($_POST['mode'] == 'update' or $_POST['mode'] == 'create' ) $form->addSubmitToForm($p['page']['form'], 'btn-warning btn-lg btn-block');
@@ -69,14 +89,8 @@ if (isset($_POST['objetID'])) {
 
 $p['page']['formJavascript']=$form->getFormJavascript();
 
-// module du formulaire
-$p['page']['formInfos']['module']=$form->getFormUniqueRawField($formIN, 'module');
 
-// extraction complémentaire via class du module
-$class='msMod'.ucfirst($p['page']['formInfos']['module']).'Forms';
-$method='getDataSupForForm_'.$formIN;
-
-if(method_exists($class,$method)) {
-  $dataSup = new $class;
-  $dataSup->$method($p);
+// méthode sépcifique au module et form : post
+if(method_exists($class,$method_post)) {
+  $form->$method_post();
 }
