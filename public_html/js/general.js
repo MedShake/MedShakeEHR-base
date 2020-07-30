cd5dfd99fd5cead701a93c27e449bb2d68828923
Indelog <contact@indelog.fr>
2020-07-30 19:16:29 +0200

    fix(): modification obtention rpps adeli sur inc-ajax-getBareCodeGenerator
    Les code rpps et adeli obtetus dans le controleur ajax n'est plus passé en
    argument avec $_POST mais directement obtenus via la valeur effective dans la
    BD à ce moment là.
    
    La modification de la box afficher et générer le code bare ne s'éfféctue
    plus qu'au moment ou la valeur rpps et adeli est effectivement modifé en base
    de donnée.

--- public_html/js/general.js
+++ public_html/js/general.js
@@ -419,6 +419,13 @@ function setPeopleData(value, patientID, typeID, source, instance) {
       },
       dataType: "json",
       success: function(data) {
+        dataTypeName = $('input[data-typeid='+typeID+']').attr('data-internalname');
+        /* Code de régénéation du code bare pour le rpps et l'adeli (dans barecodegen.js)
+           N'est activé que si la fonction est chargé (uniquement dans le template proEdit.html.twig)
+           et que l'élément modifié est le code rpps ou adeli */
+        if ((typeof retrunBareCodeBox !== 'undefined' && typeof retrunBareCodeBox === 'function') && (dataTypeName === 'rpps' || dataTypeName === 'adeli')) {
+            retrunBareCodeBox();
+        }
         glow('success', $(source));
       },
       error: function() {
