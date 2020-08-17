69fc3fe46fa02f1f1c541b44e66dfc54a49bf960
mval <val.osteopathe@gmail.com>
2020-08-17 20:19:41 +0200

    change: rewrite code for choose options
    fix: wrong function name
    
    fix: variable concatenation

--- installer/debian-bash-installer.sh
+++ installer/debian-bash-installer.sh
@@ -21,124 +21,231 @@
 #
 # @author Michaël Val
 
-# Choix des variables globales
-echo "Bienvenue, l'installation de MedShakeEHR peut commencer. Si vous avez besoin d'aide au cours de l'installation, https://c-medshakeehr.fr/doc"
-echo -e "Choix du dossier d'installation (ex: /home/ehr) : \c"
-read msehrPath
-echo -e "Choix du domaine (ex: msehr.local) : \c"
-read msehrDom
+# Choix dossier d'installation.
+selectPath() {
+    echo -e "Choix du dossier d'installation (ex: /home/ehr) : \c"
+    read msehrPath
+}
+
+# Choix des paquets à installer
+selectPackages() {
+    echo "Installation des dépendances de MedShakeEHR minimales, tapez 1 [défaut]"
+    echo "Installation de MedShakeEHR avec Orthanc (Phonecapture, Echographe ...), tapez 2"
+    echo "Ne rien installer, tapez 3"
+	echo -e "Choix : \c"
+    read selectInstall
+    case $selectInstall in
+        "1" )
+            msehrDep=$msehrMin 
+            packagesInstall ;;
+        "2" )
+            msehrDep="${msehrMin} ${extraDicom}"
+            packagesInstall ;;
+        "3" ) 
+		 ;;
+    esac 
+}
 
 # Installer les paquets
-echo "Installation de MedShakeEHR minimale, tapez 1 [defaut]"
-echo "Installation de MedShakeEHR avec Orthanc (Phonecapture, Echographe ...), tapez 2"
-read selectinstall
-msehrdep="ntp apache2 php mariadb-server ghostscript imagemagick pdftk git curl composer php-gd php-intl php-curl php-zip php-xml php-imagick php-imap php-soap php-mysql php-yaml"
-extradicom="orthanc"
-if [$selectinstall=2]
-then 
-	apt update && apt upgrade -y && apt install -y $msehrdep $extradicom
-else
-	apt update && apt upgrade -y && apt install -y $msehrdep
-fi
+packagesInstall(){
+	apt update && apt upgrade -y && apt install -y $msehrDep
+}
+
+# Choix de configurer le serveur LAMP
+selectLamp() {
+echo -e "Vous souhaitez que le serveur LAMP soit configuré par défaut, tapez 1, vous voulez configurer le serveur LAMP tapez 2, si vous l'avez déjà configuré tapez 3 : \c"
+read selectLamp
+case $selectLamp in
+    "1" )
+        certGen
+        apacheInstall
+        mariadbInstall ;;       
+    "2" )
+        selectdomain
+        certGen
+        apacheInstall
+        mariadbInstall ;;
+    "3" ) 
+	 ;;
+esac 
+}
+
+# Choix du certificat
+selectdomain() {
+    echo -e "Choix du domaine (ex: msehr.local) : \c"
+    read msehrDom
+}
 
 # Génération d'un certificat ssl
-mkdir /etc/ssl/$msehrDom
-cd /etc/ssl/$msehrDom
-openssl genrsa -out $msehrDom.key 2048
-openssl req -new -key $msehrDom.key -out $msehrDom.csr
-openssl x509 -req -days 3650 -in $msehrDom.csr -signkey $msehrDom.key -out $msehrDom.crt
+certGen() {
+    mkdir /etc/ssl/$msehrDom
+    cd /etc/ssl/$msehrDom
+    openssl genrsa -out $msehrDom.key 2048
+    openssl req -new -key $msehrDom.key -out $msehrDom.csr
+    openssl x509 -req -days 3650 -in $msehrDom.csr -signkey $msehrDom.key -out $msehrDom.crt
+}
 
 # Configuration d'Apache
+apacheInstall() {
     ## Configuration vhost http
-echo "<VirtualHost *:80>
-	ServerName $msehrDom
-	ServerAlias msehr ehr medshakeehr MedShakeEHR $msehrDom
-	DocumentRoot "$msehrPath/public_html"
-	<Directory "$msehrPath/public_html">
-		Options FollowSymLinks
-		AllowOverride all
-		Require all granted
-	</Directory>
-	ErrorLog /var/log/apache2/error.$msehrDom.log
-	CustomLog /var/log/apache2/access.$msehrDom.log combined
-</VirtualHost> 
-" > /etc/apache2/sites-available/$msehrDom.conf
+    echo "<VirtualHost *:80>
+        ServerName $msehrDom
+        ServerAlias msehr ehr medshakeehr MedShakeEHR $msehrDom
+        DocumentRoot "$msehrPath/public_html"
+        <Directory "$msehrPath/public_html">
+            Options FollowSymLinks
+            AllowOverride all
+            Require all granted
+        </Directory>
+        ErrorLog /var/log/apache2/error.$msehrDom.log
+        CustomLog /var/log/apache2/access.$msehrDom.log combined
+    </VirtualHost> 
+    " > /etc/apache2/sites-available/$msehrDom.conf
+
     ## Configuration vhost https
-echo "<VirtualHost *:443>
-	ServerName $msehrDom
-	ServerAlias msehr ehr medshakeehr MedShakeEHR $msehrDom
-	DocumentRoot "$msehrPath/public_html"
-        SSLCertificateFile /etc/ssl/$msehrDom/$msehrDom.crt
-        SSLCertificateKeyFile /etc/ssl/$msehrDom/$msehrDom.key
-	<Directory "$msehrPath/public_html">
-		Options FollowSymLinks
-		AllowOverride all
-		Require all granted
-	</Directory>
-	ErrorLog /var/log/apache2/error.$msehrDom.log
-	CustomLog /var/log/apache2/access.$msehrDom.log combined
-</VirtualHost> 
-    " >> /etc/apache2/sites-available/$msehrDom-ssl.conf
+    echo "<VirtualHost *:443>
+        ServerName $msehrDom
+        ServerAlias msehr ehr medshakeehr MedShakeEHR $msehrDom
+        DocumentRoot "$msehrPath/public_html"
+            SSLCertificateFile /etc/ssl/$msehrDom/$msehrDom.crt
+            SSLCertificateKeyFile /etc/ssl/$msehrDom/$msehrDom.key
+        <Directory "$msehrPath/public_html">
+            Options FollowSymLinks
+            AllowOverride all
+            Require all granted
+        </Directory>
+        ErrorLog /var/log/apache2/error.$msehrDom.log
+        CustomLog /var/log/apache2/access.$msehrDom.log combined
+    </VirtualHost> 
+        " >> /etc/apache2/sites-available/$msehrDom-ssl.conf
 
     ## Activation des modules Apache
-a2enmod rewrite headers ssl
+    a2enmod rewrite headers ssl
 
     ## Désactivation des sites par défaut Apache
-a2dissite 000-default.conf default-ssl.conf
+    a2dissite 000-default.conf default-ssl.conf
 
     ## Activation du vhost
-a2ensite $msehrDom $msehrDom-ssl
+    a2ensite $msehrDom $msehrDom-ssl
 
     ## Réglage php.ini
-		### vphp à adapter si autre que Debian 10
-	vphp=7.3
+	vphp=$(php -r "echo PHP_VERSION;" | cut -c1-3)
 	sed -i 's/upload_max_filesize = 2M/upload_max_filesize = 20M/' /etc/php/$vphp/apache2/php.ini
 	sed -i 's/post_max_size = 8M/post_max_size = 20M/' /etc/php/$vphp/apache2/php.ini
 	sed -i 's/max_input_vars = 1000/max_input_vars = 10000/' /etc/php/$vphp/apache2/php.ini
 
     ## Relancer Apache
-service apache2 restart
+    service apache2 restart
+}   
 
 # Configuration MariaDB
-#mysql_secure_installation
-echo -e "Choix du mot de passe root de mysql : /c"
-read mysqlrootpass
-echo -e "Choix du nom de l'admin de mysql : /c"
-read mysqladmin
-echo -e "Choix du mot de passe admin de mysql : /c"
-read mysqlpass
-mysql <<EOF
-SET PASSWORD FOR 'root'@'localhost' = PASSWORD("${mysqlrootpass}");
-DELETE FROM mysql.user WHERE User='';
-DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
-FLUSH PRIVILEGES;
-GRANT ALL ON *.* TO "${mysqladmin}"@'localhost' IDENTIFIED BY "${mysqlpass}" WITH GRANT OPTION;
+mariadbInstall() {
+    echo -e "Choix du mot de passe root de mysql : \c"
+    read mysqlrootpass
+    echo -e "Choix du nom de l'admin de mysql : \c"
+    read mysqladmin
+    echo -e "Choix du mot de passe admin de mysql : \c"
+    read mysqlpass
+    mysql <<EOF
+    SET PASSWORD FOR 'root'@'localhost' = PASSWORD("${mysqlrootpass}");
+    DELETE FROM mysql.user WHERE User='';
+    DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+    FLUSH PRIVILEGES;
+    GRANT ALL ON *.* TO "${mysqladmin}"@'localhost' IDENTIFIED BY "${mysqlpass}" WITH GRANT OPTION;
 EOF
+}
 
-# Installation de MedShakeEHR
-	## Récupération de la dernière version de MedShakeEHR
-	vlatest=$(curl --silent "https://api.github.com/repos/MedShake/MedShakeEHR-base/releases/latest" |
-		grep '"tag_name":' |                                                          
-		sed -E 's/.*"([^"]+)".*/\1/')
-	wget --no-check-certificate https://github.com/MedShake/MedShakeEHR-base/archive/$vlatest.zip -P /tmp
+# Choix de la version de MedShakeEHR
+selectVersion() {
+    echo -e "Vous voulez installer la dernière version stable tapez 1, vous voulez installer une autre version tapez 2 : \c"
+    read selectv
+    case $selectv in
+    "1" )
+        msehrLatest ;;  
+    "2" )
+        selectMsehrV ;;
+esac 
+}
 
-	## Extraction du zip
-	unzip -q -o -d /tmp /tmp/$vlatest.zip 
+# Récupération de la dernière version de MedShakeEHR
+msehrLatest() {
+    vversion=$(curl --silent "https://api.github.com/repos/MedShake/MedShakeEHR-base/releases/latest" |
+        grep '"tag_name":' |                                                          
+        sed -E 's/.*"([^"]+)".*/\1/')
+}	
 
-	## Création des répertoires de MedShakeEHR avec les bons droits
-	mkdir -p $msehrPath/public_html
-	latest=$(echo $vlatest | cut -f2 -d "v")
-	mv -f /tmp/MedShakeEHR-base-$latest/* $msehrPath
-	sed -i "1iSetEnv MEDSHAKEEHRPATH $msehrPath" $msehrPath/public_html/.htaccess
+# Récupération d'une version de MedShakeEHR
+selectMsehrV() {
+    echo -e "Tapez la version sous la forme vX.X.X : \c"
+    read vversion
+}	
 
-	## Réglages des bons droits utilisateurs
-	chown www-data:www-data -R $msehrPath
-	chmod 755 $msehrPath $msehrPath/public_html
+# Installation de MedShakeEHR     
+msehrInstall() {
+    wget --no-check-certificate https://github.com/MedShake/MedShakeEHR-base/archive/$vversion.zip -P /tmp
 
-	## Installation des dépendances avec composer
-	cd $msehrPath
-	su www-data -s/bin/bash -c 'composer install'
-	cd $msehrPath/public_html
-	su www-data -s/bin/bash -c 'composer install'
+    ## Extraction du zip
+    unzip -q -o -d /tmp /tmp/$vversion.zip 
+
+    ## Création des répertoires de MedShakeEHR avec les bons droits
+    mkdir -p $msehrPath/public_html
+    version=$(echo $vversion | cut -f2 -d "v")
+    mv -f /tmp/MedShakeEHR-base-$version/* $msehrPath
+    sed -i "1iSetEnv MEDSHAKEEHRPATH $msehrPath" $msehrPath/public_html/.htaccess
+
+    ## Réglages des bons droits utilisateurs
+    chown www-data:www-data -R $msehrPath
+    chmod 755 $msehrPath $msehrPath/public_html
+
+    ## Installation des dépendances avec composer
+    cd $msehrPath
+    su www-data -s/bin/bash -c 'composer install'
+    cd $msehrPath/public_html
+    su www-data -s/bin/bash -c 'composer install'
+}  
+
+# Choix du nettoyage
+selectRemoveInstallFiles() {
+    echo -e "Si vous souhaitez détruire les fichiers d'installation tapez 1, si vous souhaitez les conserver tapez 2"
+read selectRemove
+case $selectRemove in
+    "1" )
+        removeInstallFiles ;;
+    "2" ) 
+	 ;;
+esac
+}
+    
 # Nettoyage
-rm -r /tmp/$vlatest.zip /tmp/MedShakeEHR-base-$latest
+removeInstallFiles() {
+    rm -r /tmp/$vversion.zip /tmp/MedShakeEHR-base-$version /tmp/debian-bash-installer.sh
+}
+
+# Variables globales par défauts.
+msehrPath=/home/ehr
+msehrDom=msehr.local
+msehrMin="ntp apache2 php mariadb-server ghostscript imagemagick pdftk git curl composer php-gd php-intl php-curl php-zip php-xml php-imagick php-imap php-soap php-mysql php-yaml"
+extraDicom="orthanc"
+msehrDep=$msehrMin
+
+# Sélection de l'installation.
+echo " Bienvenue, ce script va vous guider lors de l'installation de MedShakeEHR. Si vous avez besoin d'aide au cours de l'installation : https://c-medshakeehr.fr/doc"
+echo -e "Pour commencer, si vous souhaitez installer MedShakeEHR avec ses valeurs par défaut, tapez 1 ou personnaliser l'installation, tapez 2 : \c"
+read persoInstall
+case $persoInstall in
+    "1" )
+        packagesInstall
+        certGen
+        apacheInstall
+        mariadbInstall
+        msehrLatest
+        msehrInstall
+        removeInstallFiles ;;
+    "2" )
+        selectPath
+        selectPackages
+        selectLamp
+        selectVersion
+        msehrInstall
+        selectRemoveInstallFiles ;;
+esac  
