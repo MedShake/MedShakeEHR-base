e80d562b0562ee0d74089a3ed75ace7cede731d8
Bertrand Boutillier <b.boutillier@gmail.com>
2022-11-05 11:24:39 +0100

    Fix: chemin paquet composer https://c-medshakeehr.fr/forum/viewtopic.php?p=434#p434

--- upgrade/base/sqlUpgrade_v7.1.1_v7.2.0_post.php
+++ upgrade/base/sqlUpgrade_v7.1.1_v7.2.0_post.php
@@ -6,6 +6,7 @@ $pathToComposer='php '.$p['homepath'].'composer.phar';
 file_put_contents($p['homepath'].'composer.phar', fopen("https://getcomposer.org/download/2.4.2/composer.phar", 'r'));
 
 chdir($p['config']['webDirectory']);
+exec('rm -r thirdparty/FezVrasta thirdparty/dennyferra/TypeWatch/ thirdparty/mattbryson/TouchSwipe-Jquery-Plugin/');
 exec('COMPOSER_HOME="/tmp/" '.$pathToComposer.' update 2>&1', $output);
 chdir($p['homepath']);
 exec('COMPOSER_HOME="/tmp/" '.$pathToComposer.' update 2>&1', $output);
