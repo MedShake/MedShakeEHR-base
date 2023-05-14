6dc20593c6a756a7679f035b38bbe75ad7018094
Bertrand Boutillier <b.boutillier@gmail.com>
2023-05-14 21:08:46 +0200

    Feat: run composer after upgrade

--- upgrade/base/sqlUpgrade_v7.3.0_v8.0.0_post.php
+++ upgrade/base/sqlUpgrade_v7.3.0_v8.0.0_post.php
@@ -151,3 +151,16 @@ if (is_file($homepath . '/config/plugins/faxOrangeProPlugin/aboutPluginFaxOrange
 	$value = Spyc::YAMLLoad($value);
 	msYAML::yamlFileWrite($homepath . '/config/plugins/faxOrangeProPlugin/aboutPluginFaxOrangeProPlugin.yml', $value);
 }
+
+
+// composer
+$initialDir=getcwd();
+
+$pathToComposer='php '.$p['homepath'].'composer.phar';
+file_put_contents($p['homepath'].'composer.phar', fopen("https://getcomposer.org/download/latest-stable/composer.phar", 'r'));
+
+chdir($p['config']['webDirectory']);
+exec('COMPOSER_HOME="/tmp/" '.$pathToComposer.' update 2>&1', $output);
+chdir($p['homepath']);
+exec('COMPOSER_HOME="/tmp/" '.$pathToComposer.' update 2>&1', $output);
+chdir($initialDir);
