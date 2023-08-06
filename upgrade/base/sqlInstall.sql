83de0d3880b59354cd0d6d1711fe3fe6d25d40c5
Bertrand Boutillier <b.boutillier@gmail.com>
2023-08-06 17:48:42 +0200

    v8.0.2

--- upgrade/base/sqlInstall.sql
+++ upgrade/base/sqlInstall.sql
@@ -1270,7 +1270,7 @@ INSERT IGNORE INTO `prescriptions` (`cat`, `label`, `description`, `fromID`, `to
 
 -- system
 INSERT IGNORE INTO `system` (`name`, `groupe`, `value`) VALUES
-('base', 'module', 'v8.0.1'),
+('base', 'module', 'v8.0.2'),
 ('state', 'system', 'normal');
 
 -- univtags_type
