bd2b1a806e7e036bd9e72ff060e9e32577fc56dd
marsante <github@brunoy-osteopathe.fr>
2025-05-27 17:57:39 +0200

    feat:  improve cleaning and formatting on people forms
    feat: improve people input validation

--- class/msModBaseDataSave.php
+++ class/msModBaseDataSave.php
@@ -27,166 +27,228 @@
  *
  * @author Bertrand Boutillier <b.boutillier@gmail.com>
  * @contrib fr33z00 <https://github.com/fr33z00>
+ * @contrib Michaël Val
  */
 
 class msModBaseDataSave
 {
+    /**
+     * Méthode centrale pour trimmer et traiter les valeurs avant enregistrement
+     */
+    public function processValueBeforeSave($value, $type)
+    {
+        // Remplacer les espaces insécables par des espaces standard
+        $value = preg_replace('/\xc2\xa0/', ' ', $value);
+        // Supprimer les espaces avant et après
+        $value = trim($value);
 
-	/**
-	 * La suite n'est pas documentée mais s'analyse très simplement :
-	 * le nom variable dans le nom de la method correspond au nom du type
-	 */
+        switch ($type) {
+            case 'uppercase':
+                return mb_strtoupper($value);
 
-	// dates clefs pour éviter les mauvaises suprises d'un javascript défaillant
-	public function correctionDateBeforeSave($value)
-	{
-		$value = trim($value);
-		// si absence de slash
-		if (is_numeric($value) and strlen($value) == 8) {
-			$value = $value[0] . $value[1] . '/' . $value[2] . $value[3] . '/' . $value[4] . $value[5] . $value[6] . $value[7];
-			// si année sur 2 chiffres au lieu de 4
-		} elseif (strlen($value) == 8 and substr_count($value, '/') == 2) {
-			$decompoDate = explode('/', $value);
-			if (isset($decompoDate[2]) and strlen($decompoDate[2]) == 2) {
-				$value = $decompoDate[0] . '/' . $decompoDate[1] . '/' . '20' . $decompoDate[2];
-			}
-		}
-		return $value;
-	}
-	public function tbs_DDR($value)
-	{
-		return $this->correctionDateBeforeSave($value);
-	}
-	public function tbs_ddgReel($value)
-	{
-		return $this->correctionDateBeforeSave($value);
-	}
+            case 'titlecase':
+                return mb_convert_case($value, MB_CASE_TITLE, "UTF-8");
 
-	// identité : nom en majuscule, prenom 1er lettre maj
-	public function tbs_lastname($value)
-	{
-		return  mb_strtoupper($value);
-	}
+            case 'phone':
+                $tel = preg_replace('([-_ \.])', '', $value);
+                if (strlen($tel) == 10) {
+                    return preg_replace('/(\d{2})(\d{2})(\d{2})(\d{2})(\d{2})/', '\1 \2 \3 \4 \5', $tel);
+                }
+                return $value;
 
-	public function tbs_birthname($value)
-	{
-		return  mb_strtoupper($value);
-	}
+            case 'decimal':
+                return str_replace(',', '.', $value);
 
-	public function tbs_firstname($value)
-	{
-		return mb_convert_case($value, MB_CASE_TITLE, "UTF-8");
-	}
+            case 'date':
+                if (is_numeric($value) && strlen($value) == 8) {
+                    return $value[0] . $value[1] . '/' . $value[2] . $value[3] . '/' . $value[4] . $value[5] . $value[6] . $value[7];
+                } elseif (strlen($value) == 8 && substr_count($value, '/') == 2) {
+                    $decompoDate = explode('/', $value);
+                    if (isset($decompoDate[2]) && strlen($decompoDate[2]) == 2) {
+                        return $decompoDate[0] . '/' . $decompoDate[1] . '/' . '20' . $decompoDate[2];
+                    }
+                }
+                return $value;
 
-	public function tbs_othersfirstname($value)
-	{
-		return mb_convert_case($value, MB_CASE_TITLE, "UTF-8");
-	}
+            case 'trim':
+                return $value;
 
-	// format téléphone et fax
-	public function telephoneNumberTreatBeforeSave($value)
-	{
-		$tel = preg_replace('([-_ \.])', '', $value);
-		if (strlen($tel) == 10) {
-			$tel = preg_replace('/(\d{2})(\d{2})(\d{2})(\d{2})(\d{2})/', '\1 \2 \3 \4 \5', $tel);
-			return $tel;
-		} else {
-			return $value;
-		}
-	}
+            default:
+                return $value;
+        }
+    }
 
-	public function tbs_mobilePhone($value)
-	{
-		return $this->telephoneNumberTreatBeforeSave($value);
-	}
+    /**
+     * Méthodes spécifiques pour les différents types de données
+     */
+    public function tbs_DDR($value)
+    {
+        return $this->processValueBeforeSave($value, 'date');
+    }
 
-	public function tbs_homePhone($value)
-	{
-		return $this->telephoneNumberTreatBeforeSave($value);
-	}
+    public function tbs_ddgReel($value)
+    {
+        return $this->processValueBeforeSave($value, 'date');
+    }
 
-	public function tbs_telPro($value)
-	{
-		return $this->telephoneNumberTreatBeforeSave($value);
-	}
+    public function tbs_lastname($value)
+    {
+        return $this->processValueBeforeSave($value, 'uppercase');
+    }
 
-	public function tbs_faxPro($value)
-	{
-		return $this->telephoneNumberTreatBeforeSave($value);
-	}
+    public function tbs_birthname($value)
+    {
+        return $this->processValueBeforeSave($value, 'uppercase');
+    }
 
-	public function tbs_mobilePhonePro($value)
-	{
-		return $this->telephoneNumberTreatBeforeSave($value);
-	}
+    public function tbs_firstname($value)
+    {
+        return $this->processValueBeforeSave($value, 'titlecase');
+    }
 
-	public function tbs_telPro2($value)
-	{
-		return $this->telephoneNumberTreatBeforeSave($value);
-	}
+    public function tbs_othersfirstname($value)
+    {
+        return $this->processValueBeforeSave($value, 'titlecase');
+    }
 
-	//ville : en majuscule
-	public function tbs_city($value)
-	{
-		return  mb_strtoupper($value);
-	}
+    public function tbs_mobilePhone($value)
+    {
+        return $this->processValueBeforeSave($value, 'phone');
+    }
 
-	public function tbs_villeAdressePro($value)
-	{
-		return  mb_strtoupper($value);
-	}
+    public function tbs_homePhone($value)
+    {
+        return $this->processValueBeforeSave($value, 'phone');
+    }
 
-	//Règle le problème du séparateur décimales
-	public function formatDecimalNumber($value)
-	{
-		return str_replace(',', '.', $value);
-	}
-	public function tbs_regleCheque($value)
-	{
-		return $this->formatDecimalNumber($value);
-	}
-	public function tbs_regleCB($value)
-	{
-		return $this->formatDecimalNumber($value);
-	}
-	public function tbs_regleEspeces($value)
-	{
-		return $this->formatDecimalNumber($value);
-	}
-	public function tbs_regleFacture($value)
-	{
-		return $this->formatDecimalNumber($value);
-	}
-	public function tbs_regleTarifCejour($value)
-	{
-		return $this->formatDecimalNumber($value);
-	}
-	public function tbs_regleDepaCejour($value)
-	{
-		return $this->formatDecimalNumber($value);
-	}
-	public function tbs_regleTiersPayeur($value)
-	{
-		return $this->formatDecimalNumber($value);
-	}
-	public function tbs_clairanceCreatinine($value)
-	{
-		return $this->formatDecimalNumber($value);
-	}
-	public function tbs_creatinineMgL($value)
-	{
-		return $this->formatDecimalNumber($value);
-	}
-	public function tbs_creatinineMicroMolL($value)
-	{
-		return $this->formatDecimalNumber($value);
-	}
-	public function tbs_poids($value)
-	{
-		return $this->formatDecimalNumber($value);
-	}
-	public function tbs_taillePatient($value)
-	{
-		return $this->formatDecimalNumber($value);
-	}
+    public function tbs_telPro($value)
+    {
+        return $this->processValueBeforeSave($value, 'phone');
+    }
+
+    public function tbs_faxPro($value)
+    {
+        return $this->processValueBeforeSave($value, 'phone');
+    }
+
+    public function tbs_mobilePhonePro($value)
+    {
+        return $this->processValueBeforeSave($value, 'phone');
+    }
+
+    public function tbs_telPro2($value)
+    {
+        return $this->processValueBeforeSave($value, 'phone');
+    }
+
+    public function tbs_city($value)
+    {
+        return $this->processValueBeforeSave($value, 'uppercase');
+    }
+
+	public function tbs_postalCodePerso($value)
+    {
+        return $this->processValueBeforeSave($value, 'trim');
+    }
+
+	public function tbs_streetNumber($value)
+    {
+        return $this->processValueBeforeSave($value, 'trim');
+    }
+
+    public function tbs_street($value)
+    {
+        return $this->processValueBeforeSave($value, 'trim');
+    }
+
+    public function tbs_postalCodePro($value)
+    {
+        return $this->processValueBeforeSave($value, 'trim');
+    }
+
+    public function tbs_villeAdressePro($value)
+    {
+        return $this->processValueBeforeSave($value, 'uppercase');
+    }
+
+	public function tbs_numAdressePro($value)
+    {
+        return $this->processValueBeforeSave($value, 'trim');
+    }
+
+    public function tbs_rueAdressePro($value)
+    {
+        return $this->processValueBeforeSave($value, 'trim');
+    }
+
+    public function tbs_regleCheque($value)
+    {
+        return $this->processValueBeforeSave($value, 'decimal');
+    }
+
+    public function tbs_regleCB($value)
+    {
+        return $this->processValueBeforeSave($value, 'decimal');
+    }
+
+    public function tbs_regleEspeces($value)
+    {
+        return $this->processValueBeforeSave($value, 'decimal');
+    }
+
+    public function tbs_regleFacture($value)
+    {
+        return $this->processValueBeforeSave($value, 'decimal');
+    }
+
+    public function tbs_regleTarifCejour($value)
+    {
+        return $this->processValueBeforeSave($value, 'decimal');
+    }
+
+    public function tbs_regleDepaCejour($value)
+    {
+        return $this->processValueBeforeSave($value, 'decimal');
+    }
+
+    public function tbs_regleTiersPayeur($value)
+    {
+        return $this->processValueBeforeSave($value, 'decimal');
+    }
+
+    public function tbs_clairanceCreatinine($value)
+    {
+        return $this->processValueBeforeSave($value, 'decimal');
+    }
+
+    public function tbs_creatinineMgL($value)
+    {
+        return $this->processValueBeforeSave($value, 'decimal');
+    }
+
+    public function tbs_creatinineMicroMolL($value)
+    {
+        return $this->processValueBeforeSave($value, 'decimal');
+    }
+
+    public function tbs_poids($value)
+    {
+        return $this->processValueBeforeSave($value, 'decimal');
+    }
+
+    public function tbs_taillePatient($value)
+    {
+        return $this->processValueBeforeSave($value, 'decimal');
+    }
+
+    public function tbs_nss($value)
+    {
+        return $this->processValueBeforeSave($value, 'trim');
+    }
+
+    public function tbs_nmu($value)
+    {
+        return $this->processValueBeforeSave($value, 'trim');
+    }
+
 }
