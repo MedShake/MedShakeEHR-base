fe73055c050542fd46ee9d726c7d0eb5ae688aa8
marsante <github@brunoy-osteopathe.fr>
2025-06-22 10:27:58 +0200

    fix: bad delete line

--- cron/bdpm.php
+++ cron/bdpm.php
@@ -73,6 +73,7 @@ foreach ($bdpm['dataBdpm'] as $table => $v) {
 	$file = '/tmp/' . $v['file'];
 	@unlink($file);
 	exec("curl -L " . escapeshellarg($v['url']) . " -o " . escapeshellarg($file));
+	exec("sed -i '/^[[:space:]]*$/d' ".escapeshellarg($file));
 	if (!msSQL::sqlVerifyTableExist($table)) {
 		throw new Exception("La table n'existe pas en base");
 	}
