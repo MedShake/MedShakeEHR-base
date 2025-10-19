af03cc7a27fd6c7711af00f19b989ad51411233a
marsante <46072062+marsante@users.noreply.github.com>
2025-10-19 10:28:19 +0200

    Remove duplicate contributor bad merge

--- controlers/configuration/configCheckDirectory.php
+++ controlers/configuration/configCheckDirectory.php
@@ -27,7 +27,6 @@
  * @author Bertrand Boutillier <b.boutillier@gmail.com>
  * @contrib fr33z00 <https://github.com/fr33z00>
  * @contrib Michaël Val
- * @contrib Michaël Val
  */
 
 //admin uniquement
